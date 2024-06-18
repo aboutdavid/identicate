e8ae58728a97f43df7f73cb92b08b3fa20eaf275
David M <62346025+aboutdavid@users.noreply.github.com>
2024-06-17 22:28:05 -0400

    add basic check

--- acl.js
+++ acl.js
@@ -1,7 +1,19 @@
 const config = require("./config")
 module.exports = {
-    isAllowed: function ({ user, permission }) {
+    isAllowed: function ({ user, service, permission }) {
         const user = config.users.find(user => user.id == user)
-        if (!user) return false
+        if (!user) return {
+            isAllowed: false,
+            comments: `NOTOK: USER ${user} FAILED ACL CHECK - USER DOES NOT EXIST`
+        }
+
+    
+        const filteredService = config.services.find(service => service.id == service)
+        if (!filteredService) return {
+            isAllowed: false,
+            comments: `NOTOK: USER ${user} FAILED ACL CHECK - SERVICE DOES NOT EXIST`
+        }
+
+
     }
 }
